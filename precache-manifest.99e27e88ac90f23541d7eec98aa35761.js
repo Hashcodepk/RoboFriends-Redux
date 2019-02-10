3e0f345d4739659dba6890369c9d62bc774db616
hashcodepk <hashirlives@gmail.com>
2019-02-11 03:58:58 +0500

    Updates

---
+++ precache-manifest.99e27e88ac90f23541d7eec98aa35761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends-Redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e8482bac7f761ab651c8",
+    "url": "/RoboFriends-Redux/static/js/runtime~main.e8482bac.js"
+  },
+  {
+    "revision": "cb0c6cb8069039794570",
+    "url": "/RoboFriends-Redux/static/js/main.cb0c6cb8.chunk.js"
+  },
+  {
+    "revision": "79f95ce69355eca3f695",
+    "url": "/RoboFriends-Redux/static/js/1.79f95ce6.chunk.js"
+  },
+  {
+    "revision": "cb0c6cb8069039794570",
+    "url": "/RoboFriends-Redux/static/css/main.7d106d7b.chunk.css"
+  },
+  {
+    "revision": "79f95ce69355eca3f695",
+    "url": "/RoboFriends-Redux/static/css/1.aa364bac.chunk.css"
+  },
+  {
+    "revision": "567ca98a40155f858c0d9a10c01cfe41",
+    "url": "/RoboFriends-Redux/index.html"
+  }
+];
